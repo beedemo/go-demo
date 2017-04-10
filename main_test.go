1600e6ae801ea2ab5529d2726344296b6b460f36
Kurt Madel <kmadel@mac.com>
2017-04-10 17:47:27 -0400

    added missing test file

---
+++ main_test.go
@@ -0,0 +1,236 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"gopkg.in/mgo.v2"
+	"net/http"
+	"testing"
+	"time"
+	"reflect"
+)
+
+// Setup
+
+type MainTestSuite struct {
+	suite.Suite
+}
+
+func (s *MainTestSuite) SetupTest() {
+}
+
+// RunServer
+
+func (s *MainTestSuite) Test_RunServer_InvokesHandleFuncWithHelloServer() {
+	httpHandleFuncOrig := httpHandleFunc
+	defer func() { httpHandleFunc = httpHandleFuncOrig }()
+	wasCalled := false
+	httpHandleFunc = func(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+		if pattern == "/demo/hello" && reflect.ValueOf(handler) == reflect.ValueOf(HelloServer) {
+			wasCalled = true
+		}
+	}
+
+	RunServer()
+
+	s.True(wasCalled)
+}
+
+func (s *MainTestSuite) Test_RunServer_InvokesHandleFuncWithPersonServer() {
+	httpHandleFuncOrig := httpHandleFunc
+	defer func() { httpHandleFunc = httpHandleFuncOrig }()
+	wasCalled := false
+	httpHandleFunc = func(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+		if pattern == "/demo/person" && reflect.ValueOf(handler) == reflect.ValueOf(PersonServer) {
+			wasCalled = true
+		}
+	}
+
+	RunServer()
+
+	s.True(wasCalled)
+}
+
+func (s *MainTestSuite) Test_RunServer_InvokesHandleFuncWithRandomErrorServer() {
+	httpHandleFuncOrig := httpHandleFunc
+	defer func() { httpHandleFunc = httpHandleFuncOrig }()
+	wasCalled := false
+	httpHandleFunc = func(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+		if pattern == "/demo/random-error" && reflect.ValueOf(handler) == reflect.ValueOf(RandomErrorServer) {
+			wasCalled = true
+		}
+	}
+
+	RunServer()
+
+	s.True(wasCalled)
+}
+
+func (s *MainTestSuite) Test_RunServer_InvokesListenAndServe() {
+	actual := ""
+	httpListenAndServe = func(addr string, handler http.Handler) error {
+		actual = addr
+		return nil
+	}
+
+	RunServer()
+
+	s.Equal(":8080", actual)
+}
+
+// RandomErrorServer
+
+func (s *MainTestSuite) Test_HelloServer_WritesOk() {
+	req, _ := http.NewRequest("GET", "/demo/random-error", nil)
+	w := getResponseWriterMock()
+
+	for i := 0; i <= 3; i++ {
+		RandomErrorServer(w, req)
+	}
+
+	w.AssertCalled(s.T(), "Write", []byte("Everything is still OK\n"))
+}
+
+func (s *MainTestSuite) Test_HelloServer_WritesNokEventually() {
+	req, _ := http.NewRequest("GET", "/demo/random-error", nil)
+	w := getResponseWriterMock()
+
+	for i := 0; i <= 50; i++ {
+		RandomErrorServer(w, req)
+	}
+
+	w.AssertCalled(s.T(), "Write", []byte("ERROR: Something, somewhere, went wrong!\n"))
+}
+
+// HelloServer
+
+func (s *MainTestSuite) Test_HelloServer_WritesHelloWorld() {
+	req, _ := http.NewRequest("GET", "/demo/hello", nil)
+	w := getResponseWriterMock()
+
+	HelloServer(w, req)
+
+	w.AssertCalled(s.T(), "Write", []byte("hello, world!\n"))
+}
+
+func (s *MainTestSuite) Test_HelloServer_Waits_WhenDelayIsPresent() {
+	sleepOrig := sleep
+	defer func() { sleep = sleepOrig }()
+	var actual time.Duration
+	sleep = func(d time.Duration) {
+		actual = d
+	}
+	req, _ := http.NewRequest("GET", "/demo/hello?delay=10", nil)
+	w := getResponseWriterMock()
+
+	HelloServer(w, req)
+
+	s.Equal(10*time.Millisecond, actual)
+}
+
+// PersonServer
+
+func (s *MainTestSuite) Test_PersonServer_InvokesUpsertId_WhenPutPerson() {
+	name := "Viktor"
+	upsertIdOrig := upsertId
+	defer func() { upsertId = upsertIdOrig }()
+	var actualId interface{}
+	var actualUpdate interface{}
+	upsertId = func(id interface{}, update interface{}) (info *mgo.ChangeInfo, err error) {
+		actualId = id
+		actualUpdate = update
+		return nil, nil
+	}
+	req, _ := http.NewRequest("PUT", fmt.Sprintf("/demo/person?name=%s", name), nil)
+	w := getResponseWriterMock()
+
+	PersonServer(w, req)
+
+	s.Equal(name, actualId)
+	s.Equal(&Person{Name: name}, actualUpdate)
+}
+
+func (s *MainTestSuite) Test_PersonServer_Panics_WhenUpsertIdReturnsError() {
+	upsertIdOrig := upsertId
+	defer func() { upsertId = upsertIdOrig }()
+	upsertId = func(id interface{}, update interface{}) (info *mgo.ChangeInfo, err error) {
+		return nil, fmt.Errorf("This is an error")
+	}
+	req, _ := http.NewRequest("PUT", "/demo/person?name=Viktor", nil)
+	w := getResponseWriterMock()
+
+	s.Panics(func() { PersonServer(w, req) })
+}
+
+func (s *MainTestSuite) Test_PersonServer_WritesPeople() {
+	findPeopleOrig := findPeople
+	people := []Person{
+		Person{Name: "Viktor"},
+		Person{Name: "Sara"},
+	}
+	defer func() { findPeople = findPeopleOrig }()
+	findPeople = func(res *[]Person) error {
+		*res = people
+		return nil
+	}
+	req, _ := http.NewRequest("GET", "/demo/person", nil)
+	w := getResponseWriterMock()
+
+	PersonServer(w, req)
+
+	w.AssertCalled(s.T(), "Write", []byte("Viktor\nSara"))
+}
+
+func (s *MainTestSuite) Test_PersonServer_Panics_WhenFindReturnsError() {
+	findPeopleOrig := findPeople
+	defer func() { findPeople = findPeopleOrig }()
+	findPeople = func(res *[]Person) error {
+		return fmt.Errorf("This is an error")
+	}
+	req, _ := http.NewRequest("GET", "/demo/person", nil)
+	w := getResponseWriterMock()
+
+	s.Panics(func() { PersonServer(w, req) })
+}
+
+// Suite
+
+func TestMainSuite(t *testing.T) {
+	logFatalOrig := logFatal
+	defer func() { logFatal = logFatalOrig }()
+	logFatal = func(v ...interface{}) {}
+	logPrintfOrig := logPrintf
+	defer func() { logPrintf = logPrintfOrig }()
+	logPrintf = func(format string, v ...interface{}) {}
+	httpListenAndServeOrig := httpListenAndServe
+	defer func() { httpListenAndServe = httpListenAndServeOrig }()
+	httpListenAndServe = func(addr string, handler http.Handler) error { return nil }
+	suite.Run(t, new(MainTestSuite))
+}
+
+type ResponseWriterMock struct {
+	mock.Mock
+}
+
+func (m *ResponseWriterMock) Header() http.Header {
+	m.Called()
+	return make(map[string][]string)
+}
+
+func (m *ResponseWriterMock) Write(data []byte) (int, error) {
+	params := m.Called(data)
+	return params.Int(0), params.Error(1)
+}
+
+func (m *ResponseWriterMock) WriteHeader(header int) {
+	m.Called(header)
+}
+
+func getResponseWriterMock() *ResponseWriterMock {
+	mockObj := new(ResponseWriterMock)
+	mockObj.On("Header").Return(nil)
+	mockObj.On("Write", mock.Anything).Return(0, nil)
+	mockObj.On("WriteHeader", mock.Anything)
+	return mockObj
+}
